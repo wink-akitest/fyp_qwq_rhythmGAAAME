30aa900150a2b2c712b1d8ada7c60906cef2aa04
wink-akitest <lawhiuting2023@gmail.com>
2025-10-23 06:58:29 +0800

    Update Note.cs

--- Scripts/Note.cs
+++ Scripts/Note.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 public class Note : MonoBehaviour {
     public NoteData data;
     public float speed = 5f;
+    public float targetTime; // 音符應該被擊中的時間
+    public int lane;         // 所屬的 lane (0=A,1=S,2=D,3=F)
 
     public void Init(NoteData d) {
         data = d;
+        targetTime = d.time;
+        lane = d.lane;
     }
 
     void Update() {
