6a2ea3c3e7b9ebeede656811646b3f0b9afbc433
wink-akitest <lawhiuting2023@gmail.com>
2025-10-23 05:08:34 +0800

    Update ChartLoader.cs

--- Scripts/ChartLoader.cs
+++ Scripts/ChartLoader.cs
@@ -1,26 +1,11 @@
 using UnityEngine;
 
-[System.Serializable]
-public class NoteData {
-    public float time;
-    public int lane;
-    public string type;
-    public float duration;
-}
-
-[System.Serializable]
-public class Chart {
-    public string title;
-    public string artist;
-    public float bpm;
-    public NoteData[] notes;
-}
-
 public class ChartLoader : MonoBehaviour {
     public TextAsset chartFile;
     public Chart chart;
 
     void Awake() {
         chart = JsonUtility.FromJson<Chart>(chartFile.text);
+        Debug.Log("Loaded " + chart.notes.Length + " notes");
     }
 }
