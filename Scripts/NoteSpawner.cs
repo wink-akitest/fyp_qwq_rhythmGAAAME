80c5f5ec63ff9338ca2a2b3f30c401674a4dbb66
wink-akitest <lawhiuting2023@gmail.com>
2025-10-23 06:47:30 +0800

    Update NoteSpawner.cs

--- Scripts/NoteSpawner.cs
+++ Scripts/NoteSpawner.cs
@@ -4,17 +4,29 @@ public class NoteSpawner : MonoBehaviour {
     public GameObject notePrefab;
     public AudioManager audio;
     public ChartLoader loader;
+    public Judgement judgement;
     public float spawnAhead = 2f;
+    public float songDelay = 2f;
 
     private int index = 0;
 
+    void Start() {
+        Invoke(nameof(StartSong), songDelay);
+    }
+
+    void StartSong() {
+        audio.Play();
+    }
+
     void Update() {
-        if (index < loader.chart.notes.Length) {
+        if (index < loader.chart.notes.Count) {
             float noteTime = loader.chart.notes[index].time;
             if (audio.GetSongTime() >= noteTime - spawnAhead) {
-                Vector3 pos = new Vector3(loader.chart.notes[index].lane - 4.5f, 5, 0);
+                Vector3 pos = new Vector3(loader.chart.notes[index].lane - 1.5f, 5, 0);
                 GameObject n = Instantiate(notePrefab, pos, Quaternion.identity);
-                n.GetComponent<Note>().Init(loader.chart.notes[index]);
+                Note note = n.GetComponent<Note>();
+                note.Init(loader.chart.notes[index]);
+                judgement.RegisterNote(note);
                 index++;
             }
         }
