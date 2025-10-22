7f026b8150f888141941172aa1f7ce5809535478
wink-akitest <lawhiuting2023@gmail.com>
2025-10-23 06:46:13 +0800

    Update Note.cs

--- Scripts/Note.cs
+++ Scripts/Note.cs
@@ -1,22 +1,14 @@
 using UnityEngine;
 
 public class Note : MonoBehaviour {
-    public float targetTime;
-    public int lane;
-    public string type;
-    public float duration;
+    public NoteData data;
+    public float speed = 5f;
 
-    private float speed = 5f;
-
-    public void Init(NoteData data) {
-        targetTime = data.time;
-        lane = data.lane;
-        type = data.type;
-        duration = data.duration;
+    public void Init(NoteData d) {
+        data = d;
     }
 
     void Update() {
         transform.position += Vector3.down * speed * Time.deltaTime;
-        if (transform.position.y < -5) Destroy(gameObject);
     }
 }
