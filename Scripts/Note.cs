028f302d4d1026aa4e0f15e3875fc0795d2425da
wink-akitest <lawhiuting2023@gmail.com>
2025-10-23 05:00:08 +0800

    Update Note.cs

--- Scripts/Note.cs
+++ Scripts/Note.cs
@@ -1,21 +1,11 @@
 using UnityEngine;
 
 public class Note : MonoBehaviour {
-    public float targetTime;   // 這個 note 應該被打擊的時間
-    public float speed = 5f;   // 移動速度
-    private AudioManager audio;
-
-    void Start() {
-        audio = FindObjectOfType<AudioManager>();
-    }
+    public float targetTime;
+    public float speed = 5f;
 
     void Update() {
-        // 簡單往下移動
-        transform.Translate(Vector3.down * speed * Time.deltaTime);
-
-        // 如果超過時間太久就刪掉
-        if (audio.GetSongTime() - targetTime > 1.0f) {
-            Destroy(gameObject);
-        }
+        transform.position += Vector3.down * speed * Time.deltaTime;
+        if (transform.position.y < -5) Destroy(gameObject);
     }
 }
