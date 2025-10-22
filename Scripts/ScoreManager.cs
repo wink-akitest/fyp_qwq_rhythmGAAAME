de36e486b388d59d932ee59adf8a7adc24c7885f
wink-akitest <lawhiuting2023@gmail.com>
2025-10-23 06:49:34 +0800

    Update ScoreManager.cs

--- Scripts/ScoreManager.cs
+++ Scripts/ScoreManager.cs
@@ -1,31 +1,19 @@
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour {
+    public UIManager ui;
     public int score = 0;
     public int combo = 0;
-    public UIManager ui;
 
-    public void AddScore(string judgement) {
-        switch (judgement) {
-            case "Perfect":
-                score += 1000;
-                combo++;
-                break;
-            case "Great":
-                score += 700;
-                combo++;
-                break;
-            case "Good":
-                score += 300;
-                combo = 0;
-                break;
-            case "Miss":
-                combo = 0;
-                break;
-        }
-
+    public void AddScore(int amount) {
+        score += amount;
+        combo++;
         ui.UpdateScore(score);
         ui.UpdateCombo(combo);
-        ui.ShowJudgement(judgement);
+    }
+
+    public void ResetCombo() {
+        combo = 0;
+        ui.UpdateCombo(combo);
     }
 }
