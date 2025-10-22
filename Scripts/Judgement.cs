e01b7d05cf911d2f0799fd60e6c844d251277c58
wink-akitest <lawhiuting2023@gmail.com>
2025-10-23 06:48:38 +0800

    Update Judgement.cs

--- Scripts/Judgement.cs
+++ Scripts/Judgement.cs
@@ -1,26 +1,47 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Judgement : MonoBehaviour {
     public AudioManager audio;
     public ScoreManager scoreManager;
 
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.W)) CheckHit(new int[]{1,8});
-        if (Input.GetKeyDown(KeyCode.A)) CheckHit(new int[]{6,7});
-        if (Input.GetKeyDown(KeyCode.S)) CheckHit(new int[]{4,5});
-        if (Input.GetKeyDown(KeyCode.D)) CheckHit(new int[]{2,3});
+    private Dictionary<int, List<Note>> laneNotes = new Dictionary<int, List<Note>>();
 
-        if (Input.GetMouseButtonDown(0)) scoreManager.AddScore("Perfect"); // 左鍵 slide
-        if (Input.GetMouseButtonDown(1)) scoreManager.AddScore("Perfect"); // 右鍵 slide
+    void Start() {
+        for (int i = 0; i < 4; i++) {
+            laneNotes[i] = new List<Note>();
+        }
     }
 
-    void CheckHit(int[] lanes) {
-        float t = audio.GetSongTime();
-        // TODO: 找出最近 note，比對時間差
-        float diff = 0.08f; // 假設差值
-        if (diff <= 0.05f) scoreManager.AddScore("Perfect");
-        else if (diff <= 0.1f) scoreManager.AddScore("Great");
-        else if (diff <= 0.2f) scoreManager.AddScore("Good");
-        else scoreManager.AddScore("Miss");
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.A)) CheckHit(0);
+        if (Input.GetKeyDown(KeyCode.S)) CheckHit(1);
+        if (Input.GetKeyDown(KeyCode.D)) CheckHit(2);
+        if (Input.GetKeyDown(KeyCode.F)) CheckHit(3);
+    }
+
+    void CheckHit(int lane) {
+        if (laneNotes[lane].Count > 0) {
+            Note note = laneNotes[lane][0];
+            float diff = Mathf.Abs(audio.GetSongTime() - note.data.time);
+
+            if (diff < 0.1f) {
+                scoreManager.AddScore(300);
+                Destroy(note.gameObject);
+                laneNotes[lane].RemoveAt(0);
+            } else if (diff < 0.2f) {
+                scoreManager.AddScore(100);
+                Destroy(note.gameObject);
+                laneNotes[lane].RemoveAt(0);
+            } else {
+                Debug.Log("MISS (timing off)");
+            }
+        } else {
+            Debug.Log("MISS (no note)");
+        }
+    }
+
+    public void RegisterNote(Note note) {
+        laneNotes[note.data.lane].Add(note);
     }
 }
