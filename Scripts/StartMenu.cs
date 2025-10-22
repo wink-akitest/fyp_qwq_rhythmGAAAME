c638d83167013de20ca9df1e421be7b719b7bec1
wink-akitest <lawhiuting2023@gmail.com>
2025-10-23 06:52:59 +0800

    Update StartMenu.cs

--- Scripts/StartMenu.cs
+++ Scripts/StartMenu.cs
@@ -9,11 +9,9 @@ public class StartMenu : MonoBehaviour {
 
     void Update() {
         if (!gameStarted) {
-            // 閃爍提示文字
             float alpha = Mathf.Abs(Mathf.Sin(Time.time * 2f));
             startText.color = new Color(1, 1, 1, alpha);
 
-            // 按下空白鍵開始遊戲
             if (Input.GetKeyDown(KeyCode.Space)) {
                 gameStarted = true;
                 startText.gameObject.SetActive(false);
